c20b9ee22a6083ca48b1a1b3c60382347fdbafcc
occeyan <eduardomachado.gt@gmail.com>
2025-10-04 19:16:53 +0200

    chore: reorganize project into web/ and cli/; add .gitignore

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+This file was moved to `cli/index.js` and the web UI files are in `web/`.
+
+CLI:  node cli/index.js a b c    # or run without args for interactive mode
+Web:  open web/index.html       # open in a browser
+
+Keep this file as a pointer to the project entry points.
+*/
+console.log('Use `node cli/index.js` for CLI or abra web/index.html no navegador para a UI.');
